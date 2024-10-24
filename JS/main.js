185b3a9d70862dc5776e70246cf3de7fff81ff7b
FawwazIkbar <fawwazcanaiago@gmail.com>
2024-10-24 21:41:24 +0700

    memperbaiki tampilan interaksi

--- JS/main.js
+++ JS/main.js
@@ -49,3 +49,44 @@ btnRegist.click(function () {
   registForm.addClass("active");
   loginForm.removeClass("active");
 });
+
+// view password function
+
+const loginPasw = $("#logPassword");
+const loginPaswIcon = $("#log-pass-icon");
+
+const registPasw = $("#regPassword");
+const registPaswIcon = $("#reg-pass-icon");
+
+function myLogPassword() {
+  if (loginPasw.prop("type") === "password") {
+    loginPasw.prop("type", "text");
+    loginPaswIcon.attr("name", "eye-off-outline");
+    loginPaswIcon.css("cursor", "pointer");
+  } else {
+    loginPasw.prop("type", "password");
+    loginPaswIcon.attr("name", "eye-outline");
+    loginPaswIcon.css("cursor", "pointer");
+  }
+}
+function myRegPassword() {
+  if (registPasw.prop("type") === "password") {
+    registPasw.prop("type", "text");
+    registPaswIcon.attr("name", "eye-off-outline");
+    registPaswIcon.css("cursor", "pointer");
+  } else {
+    registPasw.prop("type", "password");
+    registPaswIcon.attr("name", "eye-outline");
+    registPaswIcon.css("cursor", "pointer");
+  }
+}
+
+function changeIcon(value) {
+  if (value.length > 0) {
+    loginPaswIcon.attr("name", "eye-off-outline");
+    registPaswIcon.attr("name", "eye-off-outline");
+  } else {
+    loginPaswIcon.attr("name", "lock-closed-outline");
+    registPaswIcon.attr("name", "lock-closed-outline");
+  }
+}
